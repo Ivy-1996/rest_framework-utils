f9224f6f27e15e223c1127115207c43f2aa0588f
ivy-1996 <919624032@qq.com>
2019-12-20 11:23:15 +0800

    asd

--- setup.py
+++ setup.py
@@ -9,5 +9,24 @@ setup(
     author='ivy',
     author_email='919624032@qq.com',
     description='rest_framework utils ',
-    install_requires=read_requirements('requirements.txt')
+    install_requires=[
+        'aliyun-python-sdk-core==2.13.13',
+        'aliyun-python-sdk-core-v3==2.13.11',
+        'aliyun-python-sdk-kms==2.8.0',
+        'asgiref==3.2.3',
+        'certifi==2019.11.28',
+        'chardet==3.0.4',
+        'crcmod==1.7',
+        'Django==2.1.5',
+        'django-filter==2.2.0',
+        'djangorestframework==3.11.0',
+        'idna==2.8',
+        'jmespath==0.9.4',
+        'oss2==2.9.0',
+        'pycryptodome==3.9.4',
+        'pytz==2019.3',
+        'requests==2.22.0',
+        'sqlparse==0.3.0',
+        'urllib3==1.25.7',
+    ]
 )
