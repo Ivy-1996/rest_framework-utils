c5dfd99542ae2e5ba54682dc7d9c2aba8e080a83
ivy-1996 <919624032@qq.com>
2019-12-20 15:15:17 +0800

    asd

--- rest_framework_utils/serializers.py
+++ rest_framework_utils/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .validators import phonenum_validator
+from . import mixin
 
 
 class PhonenumField(serializers.CharField):
     def __init__(self, **kwargs):
         super(PhonenumField, self).__init__(**kwargs)
         self.validators.append(phonenum_validator)
+
+
+class Serializer(mixin.SerializerMixIn, serializers.Serializer):
+    pass
+
+
+class ModelSerializer(mixin.SerializerMixIn, serializers.ModelSerializer):
+    pass
